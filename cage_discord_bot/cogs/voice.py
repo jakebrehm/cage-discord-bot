d838bc702a121c2faccb47e22d0a0e2ed214a7d3
Jake Brehm <jake.m.brehm@gmail.com>
2020-05-07 20:08:34 -0400

    Minor change.

--- cage_discord_bot/cogs/voice.py
+++ cage_discord_bot/cogs/voice.py
@@ -56,7 +56,7 @@ class Voice(commands.Cog):
 
     def __init__(self, client):
         self.client = client
-        discord.opus.load_opus()
+        discord.opus.load_opus('opus')
 
     @commands.command(
         pass_context=True,
